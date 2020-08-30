c232ef498d2ffb11a148a5c9cd051394ee03a019
David Leigh <to.david.leigh@gmail.com>
2020-08-31 06:39:54 +1000

    - Added Rock.Lava.Shared library.

---
+++ Rock.Lava.Shared/Shortcodes/IRockShortcode.cs
@@ -0,0 +1,49 @@
+﻿// <copyright>
+// Copyright by the Spark Development Network
+//
+// Licensed under the Rock Community License (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.rockrms.com/license
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+using System.Collections.Generic;
+
+namespace Rock.Lava
+{
+
+    /// <summary>
+    /// Determines the type of tag (inline or block). Block type requires an end tag.
+    /// </summary>
+    public enum LavaElementTypeSpecifier
+    {
+        /// <summary>
+        /// A language element that consists of a single Tag.
+        /// </summary>
+        Inline = 1,
+
+        /// <summary>
+        /// A language element that consists of an opening Tag and a corresponding closing Tag.
+        /// </summary>
+        Block = 2
+    }
+
+    public interface IRockShortcode
+    {
+        string Name
+        {
+            get;
+        }
+
+        void Initialize( string tagName, string markup, IEnumerable<string> tokens );
+
+        LavaElementTypeSpecifier ElementType { get; }
+    }
+}
---
+++ Rock.Lava/Core/ILavaDataObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rock.Lava
+{
+    /// <summary>
+    /// Specifies the type is safe to be rendered by DotLiquid.
+    /// </summary>
+    public interface ILavaDataObject
+    {
+
+    }
+}
