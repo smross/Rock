fd2f10d6b02d423a09f5b18865f246940f485fa7
Matt Bronkema <matt.bronkema@sparkdevnetwork.org>
2021-03-23 17:32:16 -0400

    WIP

---
+++ RockWeb/Blocks/Administration/LogSettings.ascx
@@ -0,0 +1,64 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="LogSettings.ascx.cs" Inherits="RockWeb.Blocks.Administration.LogSettings" %>
+<asp:UpdatePanel runat="server">
+    <ContentTemplate>
+        <rock:modalalert id="mdAlert" runat="server" />
+        <div class="panel panel-block">
+            <div class="panel-heading">
+                <h1 class="panel-title"><i class="fa fa-wrench"></i> Log Settings</h1>
+            </div>
+            <div class="panel-body">
+                <asp:Panel runat="server" ID="pnlReadOnlySettings">
+                    <Rock:RockLiteral runat="server" ID="litVerbosityLevel" Label="Verbosity Level" CssClass="col-sm-6" />
+                    <Rock:RockLiteral runat="server" ID="litDomains" Label="Domains" CssClass="col-sm-6" />
+                    <asp:Button runat="server" ID="btnEdit" Text="Edit" CssClass="btn btn-primary" OnClick="btnEdit_Click" />
+                </asp:Panel>
+                <asp:Panel runat="server" ID="pnlEditSettings" Visible="false">
+                    <rock:notificationbox id="nbLoggingMessage" runat="server" notificationboxtype="Warning" title="Warning" visible="false" />
+
+                    <rock:rockradiobuttonlist id="rblVerbosityLevel" runat="server"
+                        label="Verbosity Level"
+                        help="The specified value indicates which logging level events should be written to the log file."
+                        repeatdirection="Horizontal"
+                        validationgroup="LoggingSettings"></rock:rockradiobuttonlist>
+
+                    <rock:rockcheckboxlist id="cblDomainsToLog" runat="server"
+                        label="Domains to Output"
+                        validationgroup="LoggingSettings"
+                        repeatcolumns="5"
+                        repeatdirection="Horizontal" />
+
+                    <rock:numberbox runat="server" id="txtMaxFileSize" label="Max File Size (MB)"
+                        help="The maximum size that the output file is allowed to reach before being rolled over to backup files."
+                        cssclass="input-width-md js-max-file-size"
+                        validationgroup="LoggingSettings"></rock:numberbox>
+
+                    <rock:numberbox runat="server" id="txtFilesToRetain" label="Retained Backup Files"
+                        help="The maximum number of backup files that are kept before the oldest is erased."
+                        cssclass="input-width-md js-files-to-retain"
+                        validationgroup="LoggingSettings"></rock:numberbox>
+
+                    <p>Logs could take up to <span id="maxLogSize">400</span> MB on the server's filesystem.</p>
+
+                    <div class="actions margin-t-lg">
+                        <rock:bootstrapbutton
+                            id="btnLoggingSave"
+                            runat="server"
+                            cssclass="btn btn-primary"
+                            text="Save"
+                            dataloadingtext="Saving..."
+                            validationgroup="LoggingSetting"
+                            onclick="btnLoggingSave_Click" />
+
+                        <rock:bootstrapbutton
+                            id="btnDeleteLog"
+                            runat="server"
+                            cssclass="btn btn-link"
+                            text="Delete Log"
+                            dataloadingtext="Deleting Log ..."
+                            onclick="btnDeleteLog_Click" />
+                    </div>
+                </asp:Panel>
+            </div>
+        </div>
+    </ContentTemplate>
+</asp:UpdatePanel>
---
+++ RockWeb/Blocks/Administration/LogSettings.ascx.cs
@@ -0,0 +1,109 @@
+﻿// <copyright>
+// Copyright by the Spark Development Network
+//
+// Licensed under the Rock Community License (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.rockrms.com/license
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+using System;
+using Rock;
+using Rock.Logging;
+using Rock.Security;
+using Rock.SystemKey;
+using Rock.Web.UI;
+using Rock.Web.UI.Controls;
+
+namespace RockWeb.Blocks.Administration
+{
+    /// <summary>
+    /// Used to view the <see cref="Rock.Logging.RockLogEvent"/> logged via RockLogger.
+    /// </summary>
+    [System.ComponentModel.DisplayName( "Log Settings" )]
+    [System.ComponentModel.Category( "Administration" )]
+    [System.ComponentModel.Description( "Block to edit system log settings." )]
+
+    public partial class LogSettings : RockBlock
+    {
+        #region Control Methods
+
+        /// <summary>
+        /// Raises the <see cref="E:System.Web.UI.Control.Init" /> event.
+        /// </summary>
+        /// <param name="e">An <see cref="T:System.EventArgs" /> object that contains the event data.</param>
+        protected override void OnInit( EventArgs e )
+        {
+            base.OnInit( e );
+        }
+
+        /// <summary>
+        /// Raises the <see cref="E:System.Web.UI.Control.Load" /> event.
+        /// </summary>
+        /// <param name="e">The <see cref="T:System.EventArgs" /> object that contains the event data.</param>
+        protected override void OnLoad( EventArgs e )
+        {
+            base.OnLoad( e );
+
+            if( !IsUserAuthorized( Authorization.EDIT ) )
+            {
+                btnEdit.Visible = false;
+            }
+        }
+        #endregion
+
+        protected void btnLoggingSave_Click( object sender, EventArgs e )
+        {
+            if ( !Page.IsValid )
+            {
+                return;
+            }
+
+            nbLoggingMessage.Visible = true;
+
+            var logConfig = new RockLogSystemSettings
+            {
+                LogLevel = rblVerbosityLevel.SelectedValue.ConvertToEnum<RockLogLevel>( RockLogLevel.Off ),
+                DomainsToLog = cblDomainsToLog.SelectedValues,
+                MaxFileSize = txtMaxFileSize.Text.AsInteger(),
+                NumberOfLogFiles = txtFilesToRetain.Text.AsInteger()
+            };
+
+            Rock.Web.SystemSettings.SetValue( SystemSetting.ROCK_LOGGING_SETTINGS, logConfig.ToJson() );
+
+            Rock.Logging.RockLogger.Log.ReloadConfiguration();
+
+            nbLoggingMessage.NotificationBoxType = NotificationBoxType.Success;
+            nbLoggingMessage.Title = string.Empty;
+            nbLoggingMessage.Text = "Setting saved successfully.";
+        }
+
+        protected void btnDeleteLog_Click( object sender, EventArgs e )
+        {
+            nbLoggingMessage.Visible = true;
+
+            RockLogger.Log.Delete();
+
+            nbLoggingMessage.NotificationBoxType = NotificationBoxType.Success;
+            nbLoggingMessage.Title = string.Empty;
+            nbLoggingMessage.Text = "The log files were successfully deleted.";
+        }
+
+        protected void btnEdit_Click( object sender, EventArgs e )
+        {
+            if ( IsUserAuthorized( Authorization.EDIT ) )
+            {
+                pnlEditSettings.Visible = true;
+                pnlReadOnlySettings.Visible = false;
+                HideSecondaryBlocks( true );
+            }
+        }
+    }
+}
